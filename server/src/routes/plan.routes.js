a82a5ec373fe7029e5d92faf2f3f688e8d7e4506
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-08-14 20:22:22 +0600

    plan route complete

---
+++ server/src/routes/plan.routes.js
@@ -0,0 +1,40 @@
+import express from "express";
+
+import {
+  getPlans,
+  createPlan,
+  updatePlan,
+  disablePlan,
+} from "../controllers/plan.controller.js";
+
+
+
+import { authorize } from "../middleware/role.middleware.js";
+import { protect } from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/", getPlans);
+
+router.post(
+  "/",
+  protect,
+  authorize("PLATFORM_ADMIN"),
+  createPlan
+);
+
+router.patch(
+  "/:id",
+  protect,
+  authorize("PLATFORM_ADMIN"),
+  updatePlan
+);
+
+router.patch(
+  "/:id/disable",
+  protect,
+  authorize("PLATFORM_ADMIN"),
+  disablePlan
+);
+
+export default router;
