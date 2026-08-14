e4d29b27c3077417b1c95abe847fb505d7be09a0
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-08-14 20:21:05 +0600

    memeber route complete

---
+++ server/src/routes/member.routes.js
@@ -0,0 +1,36 @@
+import express from "express";
+
+import {
+  getMembers,
+  updateMemberRole,
+  removeMember,
+} from "../controllers/member.controller.js";
+
+
+import { authorize } from "../middleware/role.middleware.js";
+import { protect } from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+router.get(
+  "/",
+  protect,
+  authorize("ORG_ADMIN"),
+  getMembers
+);
+
+router.patch(
+  "/:id/role",
+  protect,
+  authorize("ORG_ADMIN"),
+  updateMemberRole
+);
+
+router.delete(
+  "/:id",
+  protect,
+  authorize("ORG_ADMIN"),
+  removeMember
+);
+
+export default router;
