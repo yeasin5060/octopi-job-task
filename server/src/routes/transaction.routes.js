97841ce0f46ace2b1115e147ca9e49c87e6fcf92
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-08-14 20:24:21 +0600

    transaction route complete

---
+++ server/src/routes/transaction.routes.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+import {
+  getMyTransactions,
+} from "../controllers/transaction.controller.js";
+
+
+
+import { authorize } from "../middleware/role.middleware.js";
+import { protect } from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+router.get(
+  "/",
+  protect,
+  authorize("ORG_ADMIN"),
+  getMyTransactions
+);
+
+export default router;
